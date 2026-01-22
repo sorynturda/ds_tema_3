420fdf2b3dda087c81ce3aab70f3fe67d7246542
Sorin Turda <sorin.turda@co-era.com>
2026-01-22 13:54:22 +0200

    all docker images

--- build_images.sh
+++ build_images.sh
@@ -6,12 +6,12 @@ echo "Building images..."
 REPO=sorynturda/ds_tema_3
 
 docker build -t $REPO:ds-rp ./nginx
-# docker build -t $REPO:ds-auth-service ./auth
-# docker build -t $REPO:ds-user-service ./demo
-# docker build -t $REPO:ds-device-service ./demo1
-# docker build -t $REPO:ds-monitoring-service ./monitoring
-# docker build -t $REPO:ds-load-balancer ./load_balancer
-# docker build -t $REPO:ds-websocket-service ./websocket_service
-# docker build -t $REPO:ds-chat-service ./chat_service
+docker build -t $REPO:ds-auth-service ./auth
+docker build -t $REPO:ds-user-service ./demo
+docker build -t $REPO:ds-device-service ./demo1
+docker build -t $REPO:ds-monitoring-service ./monitoring
+docker build -t $REPO:ds-load-balancer ./load_balancer
+docker build -t $REPO:ds-websocket-service ./websocket_service
+docker build -t $REPO:ds-chat-service ./chat_service
 
 echo "Images built successfully."
