ba0e1e1da349ba30f7195d0bdc3a7d0bcd471d67
Sorin Turda <sorin.turda@co-era.com>
2026-01-22 13:48:13 +0200

    only one image for test

--- build_images.sh
+++ build_images.sh
@@ -3,13 +3,13 @@ set -e
 echo "Building images..."
 
 docker build -t sorynturda/ds_tema_3/ds-rp:latest ./nginx
-docker build -t sorynturda/ds_tema_3/ds-auth-service:latest ./auth
-docker build -t sorynturda/ds_tema_3/ds-user-service:latest ./demo
-docker build -t sorynturda/ds_tema_3/ds-device-service:latest ./demo1
-docker build -t sorynturda/ds_tema_3/ds-monitoring-service:latest ./monitoring
+# docker build -t sorynturda/ds_tema_3/ds-auth-service:latest ./auth
+# docker build -t sorynturda/ds_tema_3/ds-user-service:latest ./demo
+# docker build -t sorynturda/ds_tema_3/ds-device-service:latest ./demo1
+# docker build -t sorynturda/ds_tema_3/ds-monitoring-service:latest ./monitoring
 
-docker build -t sorynturda/ds_tema_3/ds-load-balancer:latest ./load_balancer
-docker build -t sorynturda/ds_tema_3/ds-websocket-service:latest ./websocket_service
-docker build -t sorynturda/ds_tema_3/ds-chat-service:latest ./chat_service
+# docker build -t sorynturda/ds_tema_3/ds-load-balancer:latest ./load_balancer
+# docker build -t sorynturda/ds_tema_3/ds-websocket-service:latest ./websocket_service
+# docker build -t sorynturda/ds_tema_3/ds-chat-service:latest ./chat_service
 
-echo "Images built successfully."
+# echo "Images built successfully."
--- push_images.sh
+++ push_images.sh
@@ -3,12 +3,12 @@ set -e
 REPO=sorynturda/ds_tema_3
 
 docker push sorynturda/ds_tema_3/ds-rp:latest
-docker push sorynturda/ds_tema_3/ds-auth-service:latest
-docker push sorynturda/ds_tema_3/ds-user-service:latest
-docker push sorynturda/ds_tema_3/ds-device-service:latest
-docker push sorynturda/ds_tema_3/ds-monitoring-service:latest
+# docker push sorynturda/ds_tema_3/ds-auth-service:latest
+# docker push sorynturda/ds_tema_3/ds-user-service:latest
+# docker push sorynturda/ds_tema_3/ds-device-service:latest
+# docker push sorynturda/ds_tema_3/ds-monitoring-service:latest
 
-docker push sorynturda/ds_tema_3/ds-load-balancer:latest
-docker push sorynturda/ds_tema_3/ds-websocket-service:latest
-docker push sorynturda/ds_tema_3/ds-chat-service:latest
+# docker push sorynturda/ds_tema_3/ds-load-balancer:latest
+# docker push sorynturda/ds_tema_3/ds-websocket-service:latest
+# docker push sorynturda/ds_tema_3/ds-chat-service:latest
 
